796411531aa17a89dd493e3eaa6aaab87ff78f12
Demo User <debian@beaglebone.localdomain>
2018-11-14 17:24:30 +0000

    upload install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+# Installing python
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt install python3 
+
+# Installing github
+sudo apt install git
+
+
+
+# Installing Adafruit_BBIO
+sudo apt install python3-pip
+git clone https://github.com/MarkAYoder/adafruit-beaglebone-io-python.git
+cd adafruit-beaglebone-io-python
+python3 setup.py install
+
+# From: http://www.scipy.org/install.html
+sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+
+sudo apt-get install python3-tk
+
